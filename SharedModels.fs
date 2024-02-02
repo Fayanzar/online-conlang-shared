172dd3f0a66bdc7efbacfc04d114f10c396271be
Fayanzar <fayanzar@gmail.com>
2024-02-02 21:10:35 +0200

    add basic authentication

--- SharedModels.fs
+++ SharedModels.fs
@@ -6,6 +6,10 @@ let routeBuilder (typeName: string) (methodName: string) =
 
 type CustomError = { errorMsg: string }
 
+type LoginInfo = { username: string; password: string }
+
+type SecurityToken = SecurityToken of string
+
 type Class = string
 type PartOfSpeech = string
 
@@ -103,65 +107,66 @@ type TermForAPI =
     }
 
 type IServer = {
+    postLogin : LoginInfo -> Async<SecurityToken>
+    postRegister : LoginInfo -> Async<unit>
+
     getLanguages : Async<list<Language>>
-    postLanguage : string -> Async<unit>
-    putLanguage : int -> string -> Async<unit>
-    deleteLanguage : int -> Async<unit>
+    postLanguage : SecurityToken -> string -> Async<SecurityToken>
+    putLanguage : SecurityToken -> int -> string -> Async<unit>
+    deleteLanguage : SecurityToken -> int -> Async<unit>
 
     getClasses : int -> Async<Map<string, seq<string>>>
-    postClass : int -> string -> Async<unit>
-    putClass : int -> string -> string -> Async<unit>
-    deleteClass : int -> string -> Async<unit>
+    postClass : SecurityToken -> int -> string -> Async<unit>
+    putClass : SecurityToken -> int -> string -> string -> Async<unit>
+    deleteClass : SecurityToken -> int -> string -> Async<unit>
 
-    postClassValue : int -> string -> string -> Async<unit>
-    putClassValue : int -> string -> string -> string -> Async<unit>
-    deleteClassValue : int -> string -> string -> Async<unit>
+    postClassValue : SecurityToken -> int -> string -> string -> Async<unit>
+    putClassValue : SecurityToken -> int -> string -> string -> string -> Async<unit>
+    deleteClassValue : SecurityToken -> int -> string -> string -> Async<unit>
 
     getSpeechParts : int -> Async<seq<string>>
-    postSpeechPart : int -> string -> Async<unit>
-    putSpeechPart : int -> string -> string -> Async<unit>
-    deleteSpeechPart : int -> string -> Async<unit>
+    postSpeechPart : SecurityToken -> int -> string -> Async<unit>
+    putSpeechPart : SecurityToken -> int -> string -> string -> Async<unit>
+    deleteSpeechPart : SecurityToken -> int -> string -> Async<unit>
 
     getTranscriptions : int -> Async<list<Transformation>>
-    postTranscription : int -> Transformation -> Async<unit>
-    putTranscription : int -> int -> Transformation -> Async<unit>
-    deleteTranscription : int -> int -> Async<unit>
+    postTranscription : SecurityToken -> int -> Transformation -> Async<unit>
+    putTranscription : SecurityToken -> int -> Transformation -> Async<unit>
+    deleteTranscription : SecurityToken -> int -> Async<unit>
 
     getTerms : int -> Async<seq<TermForAPI>>
-    postTerm : int -> TermForAPI -> Async<unit>
-    putTerm : int -> int -> TermForAPI -> Async<unit>
-    deleteTerm : int -> int -> Async<unit>
+    postTerm : SecurityToken -> int -> TermForAPI -> Async<unit>
+    putTerm : SecurityToken -> int -> TermForAPI -> Async<unit>
+    deleteTerm : SecurityToken -> int -> Async<unit>
 
-    rebuildInflections : int -> Async<unit>
+    rebuildInflections : SecurityToken -> int -> Async<unit>
 
     getAxes : int -> Async<seq<AxisForAPI>>
-    postAxisName : int -> string -> Async<unit>
-    putAxisName : int -> string -> Async<unit>
-    deleteAxisName : int -> Async<unit>
+    postAxisName : SecurityToken -> int -> string -> Async<unit>
+    putAxisName : SecurityToken -> int -> string -> Async<unit>
+    deleteAxisName : SecurityToken -> int -> Async<unit>
 
-    postAxisValue : int -> string -> Async<unit>
-    putAxisValue : int -> string -> Async<unit>
-    deleteAxisValue : int -> Async<unit>
+    postAxisValue : SecurityToken -> int -> string -> Async<unit>
+    putAxisValue : SecurityToken -> int -> string -> Async<unit>
+    deleteAxisValue : SecurityToken -> int -> Async<unit>
 
     getAxisRules : int -> int -> Async<Map<int, Rule>>
-    postAxisRule : int -> int -> Rule -> Async<unit>
-    postAxisRules : int -> int -> Rule list -> Async<unit>
-    putAxisRule : int -> Rule -> Async<unit>
-    deleteAxisRule : int -> Async<unit>
+    postAxisRules : SecurityToken -> int -> int -> Rule list -> Async<unit>
+    putAxisRule : SecurityToken -> int -> Rule -> Async<unit>
+    deleteAxisRule : SecurityToken -> int -> Async<unit>
 
     getOverrideRules : int -> int -> Async<Map<int, OverrideRule>>
-    postOverrideRule : int -> OverrideRule -> Async<unit>
-    postOverrideRules : int -> OverrideRule list -> Async<unit>
-    putOverrideRule : int -> OverrideRule -> Async<unit>
-    deleteOverrideRule : int -> Async<unit>
+    postOverrideRules : SecurityToken -> int -> OverrideRule list -> Async<unit>
+    putOverrideRule : SecurityToken -> int -> OverrideRule -> Async<unit>
+    deleteOverrideRule : SecurityToken -> int -> Async<unit>
 
     getInflections : int -> Async<seq<InflectTForAPI>>
     getInflectionsStructure : int -> Async<Map<int, Inflection>>
-    postInflection : Inflection -> Async<unit>
-    putInflection : int -> Inflection -> Async<unit>
-    deleteInflection : int -> Async<unit>
+    postInflection : SecurityToken -> Inflection -> Async<unit>
+    putInflection : SecurityToken -> int -> Inflection -> Async<unit>
+    deleteInflection : SecurityToken -> int -> Async<unit>
 
-    postPhonemeClass : int -> char -> char option -> Async<unit>
-    putPhonemeClass : int -> char -> char -> Async<unit>
-    deletePhonemeClass : int -> char -> Async<unit>
+    postPhonemeClass : SecurityToken -> int -> char -> char option -> Async<unit>
+    putPhonemeClass : SecurityToken -> int -> char -> char -> Async<unit>
+    deletePhonemeClass : SecurityToken -> int -> char -> Async<unit>
 }
